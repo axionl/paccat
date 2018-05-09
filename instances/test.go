59d268fd2f3983a88d5e0e64d950ee836b4c9a10
axionl <axionl@aosc.io>
2018-05-09 15:14:04 +0800

    Update: process rawArgs.

--- instances/test.go
+++ instances/test.go
@@ -2,22 +2,34 @@ package instances
 
 import (
 	"errors"
-	"fmt"
+	"paccat/local"
 	"paccat/logger"
-	"paccat/net"
 	"paccat/packing"
+	"strings"
 )
 
 func Test(rawArgs []string) {
-	if rawArgs == nil {
-		dir = "."
-	} else if len(rawArgs) == 1 {
-		dir = rawArgs[0]
-	} else {
-		err := errors.New("don't make more than one package at a time")
-		logger.CheckErr("Making Package: ", err)
+	var packagePath string
+	quietMode := false
+
+	for _, item := range rawArgs {
+		if strings.Contains("-q", item) || strings.Contains("--quiet", item) {
+			quietMode = true
+			if len(rawArgs) > 2 {
+				err := errors.New("don't make more than one package at a time")
+				logger.CheckErr("Making Package: ", err)
+			}
+		} else {
+			packagePath = item
+		}
 	}
-	fmt.Println(dir)
-	pkg := net.CheckNewVersion(dir)
-	packing.AutoPack(pkg, true)
+
+	if len(packagePath) == 0 {
+		packagePath = "."
+	}
+
+	pkg, err := local.Parse(packagePath)
+	logger.CheckErr("Parse PKGBUILD: ", err)
+
+	packing.AutoPack(pkg, quietMode)
 }
